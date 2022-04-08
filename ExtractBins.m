1de6337ea5743cb4ef7e5d843762ae971cead0f5
barbaalba <39873583+barbaalba@users.noreply.github.com>
2022-04-08 11:10:49 +0200

    Create ExtractBins.m
    To extract bin edge and corresponding probability

---
+++ ExtractBins.m
@@ -0,0 +1,14 @@
+function [binedge, prob] = ExtractBins(fig)
+% To Extract bin edge and corresponding probabilty of each bin from
+% experimental data
+%
+% fig: The given figure generated with histogram (fig = gcf;)
+% 
+
+axObjs = fig.Children;
+dataObjs = axObjs.Children;
+
+prob = dataObjs.Values;
+binedge = dataObjs.BinEdges;
+
+end
